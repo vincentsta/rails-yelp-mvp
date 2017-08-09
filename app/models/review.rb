5cd10cefa4493cdaf315cc69ed1d4d1f81dd9c45
Vincent Stammbach <vincentsta.dev@gmail.com>
2017-08-09 15:21:52 +0200

    reviews ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
 
   validates :restaurant, presence: true
   validates :content, presence: true
-  validates :rating, inclusion: { in: (0..5), allow_nil: false }, numericality: { only_integer: true }
+  validates :rating, presence: true, inclusion: { in: (0..5), allow_nil: false }, numericality: { only_integer: true }
 end
